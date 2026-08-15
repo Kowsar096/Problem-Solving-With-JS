f2fedcb618ba7f74fea17f1b481e0640552b7c05
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-16 01:13:26 +0600

    Add array looping example and disable inline suggestions in VSCode settings

---
+++ JS-array/array-looping.js
@@ -0,0 +1,14 @@
+const fruits = ['Apple', 'Banana', 'Orange', 'Watermelon'];
+console.log('----For Loop----')
+for (let i = 0; i< fruits.length; i++) {
+    // console.log(i);
+    console.log(fruits[i]);
+}
+
+console.log('----While Loop----')
+
+let k = 0;
+ while (k < fruits.length){
+    console.log(fruits[k]);
+    k++;
+ }
