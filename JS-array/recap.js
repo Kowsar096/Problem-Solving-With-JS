d8c58b3d99cc3bb3cc1be496fd8c346cbb6a1521
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-17 23:59:25 +0600

    Add a recap file for overall folder.

---
+++ JS-array/recap.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+const book = ['physics', 'javascript', 'css', 'math'];
+
+for (let i = 0; i< book.length; i++){
+    console.log(book[i]);
+}
+
+//reverse
+
+const rev_book = book.reverse();
+console.log(rev_book);
+
+// sort
+
+const sorted_book = books.sort(); // string type sort
+
+// 
+
+const gadgets = [
+    {name: 'laptop', price: 35000, color: 'Black', brand: 'Lenovo'},
+    {name: 'laptop', price: 35000, color: 'Black', brand: 'Lenovo'},
+    {name: 'laptop', price: 35000, color: 'Black', brand: 'Lenovo'}
+]
+
+// 2d array
+
+const exam_marks = [
+    [98, 87, 45, 33, 80],
+    [54, 65, 55, 30, 69],
+    [64, 68, 75, 39, 60],
+    [53, 66, 58, 35, 68]
+]
