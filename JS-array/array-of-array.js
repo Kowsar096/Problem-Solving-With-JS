2aab2c4fbe8bcc8aba93274276f96640ccbf6532
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-17 23:08:01 +0600

    Add 2D array example with console logging

---
+++ JS-array/array-of-array.js
@@ -0,0 +1,14 @@
+// 2d array
+
+const number = [1, 2, 3, 4, 5];
+
+const exam_marks = [
+    [98, 87, 45, 33, 80],
+    [54, 65, 55, 30, 69],
+    [64, 68, 75, 39, 60],
+    [53, 66, 58, 35, 68]
+]
+console.log(number[0]);
+console.log(exam_marks[0]); 
+console.log(exam_marks[0][0]);  //specific array value 
+console.log(exam_marks); // full array of array
