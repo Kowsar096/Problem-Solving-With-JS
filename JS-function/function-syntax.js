152a28ee04cbb9bec78563d9ed3dab3779646350
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-18 23:04:33 +0600

    Refactor function call syntax and add sum function example

---
+++ JS-function/function-syntax.js
@@ -0,0 +1,13 @@
+// function syntax
+
+console.log('------function Syntax------');
+
+function sum(x,y){
+    const result = x + y;
+    return result;
+
+}
+
+const total = sum(5,3);
+
+console.log(total);
--- JS-function/function.js
+++ JS-function/function.js
@@ -10,4 +10,4 @@ function turnOff(){
 
 // call the function 
 
-turnOff();
+turnOff()
