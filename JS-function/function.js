d42a6c88c4cbcfbbc6f171e391d8677a352d4d0c
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-18 22:47:23 +0600

    Add function to simulate turning off a light switch

---
+++ JS-function/function.js
@@ -0,0 +1,13 @@
+const number = 45;
+
+// function declaration
+
+function turnOff(){
+    console.log("Stand Up");
+    console.log('Walk towards the switch');
+    console.log('click the switch');
+}
+
+// call the function 
+
+turnOff();
