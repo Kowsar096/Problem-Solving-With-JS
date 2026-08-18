d1dc86ed886703b879bd815e7c88904c739945ef
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-19 00:22:13 +0600

    Add functions to double a number and calculate the difference between two ages

---
+++ JS-function/arguments.js
@@ -0,0 +1,24 @@
+
+
+function doubleIt(number) {
+    const doubled = number * 2;
+    console.log(number, doubled);
+
+}
+
+doubleIt(15);
+
+// we can call it using variable
+
+const number = 55;
+doubleIt(number);
+
+function difference (num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2, 'Difference is: ', diff);
+}
+
+const fatherAge = 55;
+const myAge = 26;
+
+difference(fatherAge, myAge);
