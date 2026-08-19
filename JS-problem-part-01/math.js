237373dd5a1c20026ae821ccd673ec6401caf95a
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-20 01:45:27 +0600

    Add mathematical functions for min, max, PI, absolute value, rounding, and random number generation

---
+++ JS-problem-part-01/math.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+// minimum number
+const min = Math.min(14, 1, 5, 15, 4, 15, 4, 15, 41, 51, 154, 15, 1, 51, 51);
+console.log(min);
+//maximum number
+const max = Math.max(14, 1, 5, 15, 4, 15, 4, 15, 41, 51, 154, 15, 1, 51, 51);
+console.log(max);
+// pi value
+
+console.log(Math.PI);
+
+console.log(Math.abs(-88));
+
+// round-figure 
+console.log(Math.round(2.25));
+console.log(Math.round(2.55));
+
+//math. random
+
+console.log(Math.random());
