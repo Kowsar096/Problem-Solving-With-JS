7ba5585497363229f3aabe2cd13cee16402504a4
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-20 02:07:08 +0600

    Add date manipulation and swapping values examples

---
+++ JS-problem-part-01/date.js
@@ -0,0 +1,12 @@
+const today = new Date();
+console.log(today);
+
+const date = new Date('2026-10-19');
+console.log(date.getMonth());
+
+const specificDate = new Date( 2091, 0, 26);
+console.log(specificDate)
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleDateString('en-GB'));
+
+// unix epoc
